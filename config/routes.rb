97c0ddfe2d55825f7011520f6ae47c1d3f825621
JulienLavau <julienlavau17@gmail.com>
2023-11-24 11:58:15 +0100

    123

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :lists, only: %i[index show new create]
+  root "lists#index"
+  resources :lists, only: %i[show new create]
 
   resources :bookmarks, only: %i[create new index]
   resources :bookmarks, only: :destroy
